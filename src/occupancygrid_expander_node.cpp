da7573a1c8df8840cf284ca515e390d4fa0ec5ec
Tatsuhiro Ikebe <tatuhiro315@ezweb.ne.jp>
2021-09-27 23:12:21 +0900

    Refactor/c++ (#4)
    * Refactor c++
    
    * Add param

--- src/occupancygrid_expander_node.cpp
+++ src/occupancygrid_expander_node.cpp
@@ -1,54 +1,42 @@
 #include <ros/ros.h>
-#include "nav_msgs/OccupancyGrid.h"
+#include <nav_msgs/OccupancyGrid.h>
 #include <nav_msgs/GetMap.h>
 #include <nav_msgs/Odometry.h>
 
-#include "grid_map_ros/GridMapRosConverter.hpp"
-#include "grid_map_ros/GridMapMsgHelpers.hpp"
+#include <grid_map_ros/GridMapRosConverter.hpp>
+#include <grid_map_ros/GridMapMsgHelpers.hpp>
 
-std::vector<uint8_t> realMap;     //Real world map.
+int width =         1000;
+int height =        1000;
+int8_t value =      -1;
+float resolution =  0.1;
+std::string frame_id = "odom";
 
 ros::Publisher map_pub;
 
-int width =         400;
-int height =        200;
-float resolution =  0.1;
-
-double x;
-double y;
-std::string frame_id;
+double position_x;
+double position_y;
 
 void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
   std_msgs::Header header = msg->header;
   nav_msgs::MapMetaData info = msg->info;
-  // ROS_INFO("Got map %d %d", info.width, info.height);
 
   nav_msgs::OccupancyGrid new_map;
 
-  new_map.header.frame_id = "odom";
+  new_map.header.frame_id = frame_id;
   new_map.info.width = width;
   new_map.info.height = height;
   new_map.info.resolution = resolution;
-  new_map.info.origin.position.x = x;
-  new_map.info.origin.position.y = y;
+  new_map.info.origin.position.x = -((width+(width-msg->info.width*2))*resolution*0.25) + position_x;
+  new_map.info.origin.position.y = -((height+(height-msg->info.height*2))*resolution*0.25) + position_y;
   new_map.info.origin.position.z = 0.0;
   new_map.info.origin.orientation.z = 0.0;
   new_map.info.origin.orientation.w = 1.0;
-  new_map.data.assign(width * height, -1);
+  new_map.data.assign(width * height, value);
 
   double h = height*resolution*0.5 - msg->info.height*resolution*0.5;
-  // std::cout << height*resolution*0.5 << ", " << msg->info.height*resolution*0.5 << ", " << h << "\n";
 
-  int cnt = 0;
-  // std::vector<int8_t> msg_data;
-  // msg_data.resize(msg->info.width*msg->info.height);
-  // for (int i = 0; i < msg->info.width; i++){
-  //   for (int j = 0; j < msg->info.height; j++){
-  //     std::cout << msg->data[i + msg->info.width *j];
-  //     msg_data.push_back(msg->data[cnt++]);
-  //   }
-  // }
   int map_index = 0;
   int msg_index = 0;
   for (double i = 0; i < height; i++){
@@ -56,36 +44,27 @@ void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
       if(h<i*resolution && height*resolution-h>i*resolution){
         if(i*width+(width-msg->info.width)*0.5<=map_index && 
             (i+1)*width-(width-msg->info.width)*0.5>=map_index){
-          new_map.data[map_index] = msg->data[(j-100) + ((i-50)) * msg->info.width];
-          // std::cout << (j-100) + (msg->info.height - (i-50) - 1) * msg->info.width << "\n";
+          new_map.data[map_index] = 
+            msg->data[(j-(width-msg->info.width)*0.5) + (i-(height-msg->info.height)*0.5) * msg->info.width];
           msg_index++;
         }
       }
       map_index++;
-      // std::cout << j*resolution << "\n";
     }
   }
   map_pub.publish(new_map);
 }
 
-void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
-{
-  // x = msg->pose.pose.orientation.z;
-  // y = msg->pose.pose.orientation.w;
-  // ROS_INFO("Got map %f %f", x, y);
-}
-
-void gridCallback(const grid_map_msgs::GridMap& msg)
+void gridCallback(const grid_map_msgs::GridMap::ConstPtr& msg)
 {
   grid_map::GridMap map;
-  grid_map::GridMapRosConverter::fromMessage(msg, map);
+  grid_map::GridMapRosConverter::fromMessage(*msg, map);
   
   map.getLength();
   grid_map::Position position = map.getPosition() - 0.5 * map.getLength().matrix();
-  x = -(width*resolution*0.25) + position.x();
-  y = -(height*resolution*0.25) + position.y();
-  // frame_id = map.getFrameId();
-  // std::cout << x << y << "\n";
+
+  position_x = position.x();
+  position_y = position.y();
 }
 
 int main(int argc, char **argv)
@@ -94,8 +73,8 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   map_pub = n.advertise<nav_msgs::OccupancyGrid>("occupancygrid_expander_map",10);
+ 
   ros::Subscriber map_sub = n.subscribe("/traversability_map_visualization/step_map",1 , mapCallback);
-  ros::Subscriber odom_sub = n.subscribe("/t265/odom/sample",1 , odomCallback);
   ros::Subscriber grid_sub = n.subscribe("/elevation_mapping/elevation_map",1 , gridCallback);
   
   ros::spin();
